3d4ef7fd3434bf8e1d47e1f25b5f84c78f66f82c
jthielman <john.thielman@gmail.com>
2020-05-09 12:02:01 -0500

    Add project files.

---
+++ Chinook/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Chinook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chinook.Controllers
+{
+    [Route("api/Customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/Customer/brazil
+        [HttpGet("{country}")]
+        public IActionResult GetByCountry(string country)
+        {
+            var repo = new CustomerRepository();
+            var customers = repo.GetByCountry(country);
+
+            if (!customers.Any())
+            {
+                return NotFound();
+            }
+            return Ok(customers);
+        }
+
+        // GET: api/Customer/brazil/invoices
+        [HttpGet("{country}/invoices")]
+        public IActionResult GetInvoicesByCountry(string country)
+        {
+            var repo = new CustomerRepository();
+            var invoices = repo.GetInvoicesByCountry(country);
+
+            if (!invoices.Any())
+            {
+                return NotFound();
+            }
+            return Ok(invoices);
+        }
+    }
+}
---
+++ Chinook/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chinook.Data
+{
+    public class InvoiceCustomer
+    {
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public int InvoiceId { get; internal set; }
+        public DateTime InvoiceDate { get; internal set; }
+        public string BillingCountry { get; internal set; }
+    }
+}
